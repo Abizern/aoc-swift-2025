bb6681a3c9e0a1e67d48bf4f0b566c39b6e28f8e
Abizer Nasir <abizern@mac.com>
2025-12-02 21:58:04 +0000

    Setup for day3

---
+++ Tests/Day03Tests.swift
@@ -0,0 +1,39 @@
+import Testing
+
+@testable import AdventOfCode
+
+@Suite("Day03 Tests")
+struct Day03Tests {
+  @Suite("Parser Tests")
+  struct ParserTests {
+    @Test("Test parser implementation")
+    func parseInput() {
+      let day = Day03(data: testInput)
+    }
+  }
+
+  @Suite("Tests on sample inputs")
+  struct SolutionsTests {
+    let day = Day03(data: testInput)
+
+    @Test("Part1 example")
+    func testPart1() async throws {
+      await withKnownIssue {
+        let result = try await day.part1()
+        #expect(result == 10)
+      }
+    }
+
+    @Test("Part2 example")
+    func testPart2() async throws {
+      await withKnownIssue {
+        let result = try await day.part2()
+        #expect(result == 10)
+      }
+    }
+  }
+}
+
+private let testInput =
+  """
+  """
