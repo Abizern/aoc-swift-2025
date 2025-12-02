05ffedc27f0a7bf00277c1e5013b8b299a80d36b
Abizer Nasir <abizern@mac.com>
2025-12-02 09:37:21 +0000

    Setup for day 2

---
+++ Sources/Day02.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+struct Day02: AdventDay, Sendable {
+  let data: String
+  let day = 2
+  let puzzleName: String = "--- Day 2: Gift Shop ---"
+
+  init(data: String) {
+    self.data = data
+  }
+
+  func part1() async throws -> Int {
+    0
+  }
+}
+
+extension Day02 {}
+
+// Parsing
+extension Day02 {}
---
+++ Tests/Day02Tests.swift
@@ -0,0 +1,41 @@
+import Testing
+
+@testable import AdventOfCode
+
+@Suite("Day02 Tests")
+struct Day02Tests {
+  @Suite("Parser Tests")
+  struct ParserTests {
+    @Test("Test parser implementation")
+    func parseInput() {
+      let day = Day02(data: testInput)
+
+    }
+  }
+
+  @Suite("Tests on sample inputs")
+  struct SolutionsTests {
+    let day = Day02(data: testInput)
+
+    @Test("Part1 example")
+    func testPart1() async throws {
+      await withKnownIssue {
+        let result = try await day.part1()
+        #expect(result == 10)
+      }
+    }
+
+    @Test("Part2 example")
+    func testPart2() async throws {
+      await withKnownIssue {
+        let result = try await day.part2()
+        #expect(result == 10)
+      }
+    }
+  }
+}
+
+private let testInput =
+  """
+  11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
+  """
