4d8454ec88fbed217983cbc73b9046908c2093cb
Abizer Nasir <abizern@mac.com>
2025-12-05 03:07:23 +0000

    Setup for day5

---
+++ Sources/Day05.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+struct Day05: AdventDay, Sendable {
+  let data: String
+  let day = 5
+  let puzzleName: String = "--- Day 0: Placeholder! ---"
+
+  init(data: String) {
+    self.data = data
+  }
+
+  func part1() async throws -> Int {
+    0
+  }
+}
+
+extension Day05 {}
+
+// Parsing
+extension Day05 {}
