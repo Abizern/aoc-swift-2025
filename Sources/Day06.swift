1e040a8c86f656ca5221afc48b63305677cc8aa2
Abizer Nasir <abizern@mac.com>
2025-12-05 16:42:50 +0000

    Setup for day6

---
+++ Sources/Day06.swift
@@ -0,0 +1,21 @@
+import AoCCommon
+import Foundation
+
+struct Day06: AdventDay, Sendable {
+  let data: String
+  let day = 6
+  let puzzleName: String = "--- Day 6: Placeholder! ---"
+
+  init(data: String) {
+    self.data = data
+  }
+
+  func part1() async throws -> Int {
+    0
+  }
+}
+
+extension Day06 {}
+
+// Parsing
+extension Day06 {}
---
+++ Tests/Day06Tests.swift
@@ -0,0 +1,39 @@
+import Testing
+
+@testable import AdventOfCode
+
+@Suite("Day06 Tests")
+struct Day06Tests {
+  @Suite("Parser Tests")
+  struct ParserTests {
+    @Test("Test parser implementation")
+    func parseInput() {
+      let day = Day06(data: testInput)
+    }
+  }
+
+  @Suite("Tests on sample inputs")
+  struct SolutionsTests {
+    let day = Day06(data: testInput)
+
+    @Test("Part1 example")
+    func testPart1() async throws {
+      await withKnownIssue {
+        let result = try await day.part1()
+        #expect(result == 10)
+      }
+    }
+
+    @Test("Part2 example")
+    func testPart2() async throws {
+      await withKnownIssue {
+        let result = try await day.part2()
+        #expect(result == 10)
+      }
+    }
+  }
+}
+
+private let testInput =
+  """
+  """
