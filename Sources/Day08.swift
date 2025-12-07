c2c5b9f89f08cd43463ab10ceb258788c62ba1ce
Abizer Nasir <abizern@mac.com>
2025-12-07 15:28:04 +0000

    Setup for day8

---
+++ Sources/Day08.swift
@@ -0,0 +1,21 @@
+import AoCCommon
+import Foundation
+
+struct Day08: AdventDay, Sendable {
+  let data: String
+  let day = 8
+  let puzzleName: String = "--- Day 0: Placeholder! ---"
+
+  init(data: String) {
+    self.data = data
+  }
+
+  func part1() async throws -> Int {
+    0
+  }
+}
+
+extension Day08 {}
+
+// Parsing
+extension Day08 {}
