399ed05ba453485b707d4678ca56f1d9898818b4
Abizer Nasir <abizern@mac.com>
2025-12-07 10:17:08 +0000

    Setup and parsing for day7

--- Sources/Day07.swift
+++ Sources/Day07.swift
@@ -1,12 +1,17 @@
+import AoCCommon
 import Foundation
 
 struct Day07: AdventDay, Sendable {
   let data: String
+  let grid: Grid<Character>
+  let startCell: Cell
   let day = 7
-  let puzzleName: String = "--- Day 0: Placeholder! ---"
+  let puzzleName: String = "--- Day 7: Laboratories --- "
 
   init(data: String) {
     self.data = data
+    grid = try! Grid(rows: data.characterLines())
+    startCell = grid.firstCell(for: "S")!
   }
 
   func part1() async throws -> Int {
