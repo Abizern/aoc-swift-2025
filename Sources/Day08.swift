3744946455ef7e1a64142c473140c698c708369a
Abizer Nasir <abizern@mac.com>
2025-12-08 22:09:05 +0000

    Solve Day8 part 1
    Actually, solve it with tests, already got a correct solution with rough initial code

--- Sources/Day08.swift
+++ Sources/Day08.swift
@@ -1,21 +1,106 @@
+import Accelerate
 import AoCCommon
+import Collections
 import Foundation
 
 struct Day08: AdventDay, Sendable {
-  let data: String
+  let junctions: [Vector3D]
+  let closestPairs: [(Vector3D, Vector3D)]
   let day = 8
-  let puzzleName: String = "--- Day 0: Placeholder! ---"
+  let puzzleName: String = "--- Day 8: Playground ---"
 
   init(data: String) {
-    self.data = data
+    junctions = try! NumberLines().parse(data).map(Vector3D.init)
+    closestPairs = Self.generateDistances(junctions)
+      .sorted { $0.1 < $1.1 }
+      .map(\.0)
   }
 
   func part1() async throws -> Int {
-    0
+    let pairs = closestPairs.prefix(1000)
+    let chainSizes = chainSizeList(pairs).sorted(by: >).prefix(3).reduce(into: 1, *=)
+
+    return chainSizes
   }
 }
 
-extension Day08 {}
+extension Day08 {
+  static func generateDistances(_ junctions: [Vector3D]) -> [((Vector3D, Vector3D), Int)] {
+    var accumulator: [((Vector3D, Vector3D), Int)] = []
+    var tails = junctions[...]
+    guard !tails.isEmpty else { return [] }
 
-// Parsing
-extension Day08 {}
+    var head = tails.removeFirst()
+
+    while !tails.isEmpty {
+      for tail in tails {
+        accumulator.append(((head, tail), head.squaredDistanceTo(tail)))
+      }
+      head = tails.removeFirst()
+    }
+
+    return accumulator
+  }
+
+  func chainSizeList(_ pairs: Array<(Vector3D, Vector3D)>.SubSequence) -> [Int] {
+    var adjacency = Dictionary(uniqueKeysWithValues: junctions.map { ($0, Set<Vector3D>()) })
+
+    for (a, b) in pairs {
+      adjacency[a, default: []].insert(b)
+      adjacency[b, default: []].insert(a)
+    }
+
+    var visited: Set<Vector3D> = []
+    var sizes: [Int] = []
+
+    for junction in junctions {
+      guard !visited.contains(junction) else { continue }
+
+      var queue: Deque<Vector3D> = [junction]
+      var currentSize = 0
+
+      while let current = queue.popFirst() {
+        // Skip if we queued this node multiple times
+        guard !visited.contains(current) else { continue }
+
+        visited.insert(current)
+        currentSize += 1
+
+        for connection in adjacency[current, default: []] {
+          if !visited.contains(connection) {
+            queue.append(connection)
+          }
+        }
+      }
+
+      sizes.append(currentSize)
+    }
+
+    return sizes
+  }
+}
+
+extension Day08 {
+  struct Vector3D: Hashable, CustomStringConvertible {
+    var description: String {
+      "(\(x), \(y), \(z))"
+    }
+
+    let x: Int
+    let y: Int
+    let z: Int
+
+    init(_ list: [Int]) {
+      x = list[0]
+      y = list[1]
+      z = list[2]
+    }
+
+    func squaredDistanceTo(_ other: Vector3D) -> Int {
+      let dx = x - other.x
+      let dy = y - other.y
+      let dz = z - other.z
+      return dx * dx + dy * dy + dz * dz
+    }
+  }
+}
--- Tests/Day08Tests.swift
+++ Tests/Day08Tests.swift
@@ -4,24 +4,15 @@ import Testing
 
 @Suite("Day08 Tests")
 struct Day08Tests {
-  @Suite("Parser Tests")
-  struct ParserTests {
-    @Test("Test parser implementation")
-    func parseInput() {
-      let day = Day08(data: testInput)
-    }
-  }
-
   @Suite("Tests on sample inputs")
   struct SolutionsTests {
     let day = Day08(data: testInput)
 
     @Test("Part1 example")
-    func testPart1() async throws {
-      await withKnownIssue {
-        let result = try await day.part1()
-        #expect(result == 10)
-      }
+    func part1() async throws {
+      let pairs = day.closestPairs.prefix(10)
+      let result = day.chainSizeList(pairs).prefix(3).reduce(into: 1, *=)
+      #expect(result == 40)
     }
 
     @Test("Part2 example")
@@ -36,4 +27,24 @@ struct Day08Tests {
 
 private let testInput =
   """
+  162,817,812
+  57,618,57
+  906,360,560
+  592,479,940
+  352,342,300
+  466,668,158
+  542,29,236
+  431,825,988
+  739,650,466
+  52,470,668
+  216,146,977
+  819,987,18
+  117,168,530
+  805,96,715
+  346,949,466
+  970,615,88
+  941,993,340
+  862,61,35
+  984,92,344
+  425,690,689
   """
